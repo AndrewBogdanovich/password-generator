405307cd8a3820343e59efc8966bad0425b1a509
andrewbohdanovich <andrewbohdanovich@gmail.com>
2024-10-03 19:19:36 +0300

    PG-1 Добавил в генератор поролей символьную генерацию

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,10 +11,17 @@ class PasswordGenerator() {
         const val PASSWORD_LENGTH = 8
     }
 
+
     fun generatePassword(): String{
         var password = ""
         for (i in 1..PASSWORD_LENGTH) {
-            password += (Random.nextInt(0,9).toString())
+            if (i % 2 == 0) {
+                val charRange = ('A'..'Z').random()
+                password += charRange
+            }
+            else {
+                password += (Random.nextInt(0,9).toString())
+            }
         }
         return password
     }
